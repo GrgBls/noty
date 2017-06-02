6019d11782bc69d19a8b0c25114d940edb89d5ec
Giorgos Balis <gmpalis6@gmail.com>
2017-06-02 13:46:00 +0200

    Update v1.5

--- Noty.py
+++ Noty.py
@@ -1,19 +1,24 @@
+from tkinter import *
+
+
 def cli():
-	import time 
-	from Tkinter import * #imports time library and Tkinter library.
-
-    
-
-
-	print("Welcome to Noty.You can now create sticky notes, easily.") #Prints the text.
-	time.sleep(2)
-	x=raw_input("Type your notes here: ") #Asks for user input.
-	time.sleep(1)
+	import time
+	print("Welcome to Noty.You can now create sticky notes, easily.")
+	time.sleep(1.5)
+	x=input("Type your notes here: ")
+	time.sleep(1) 
+	#time.sleep prevents GUI from popping up before it receives input.
 	root = Tk()
-	root.title("Noty") #Changes the GUI title to Noty.
-	root.geometry("400x400+50+50") #Changes the width and the height of the GUI.
-	Label = Label(root, text=x) #Prints the previous input(x)
-	Label.pack() #Packs the label
+	root.title("Noty")
+	root.geometry("300x300")
+	#changes the width and height of the GUI.
+	label = Label(root, text=x)
+	#prints the input
+	label.pack()
 	root.mainloop()
+
+
+
+
 
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'Noty',
-    version = '0.1.1',
+    version = '0.1.5',
     description = 'Creating sticky notes has never been easier',
     author = 'GrgBls',
     author_email = 'gmpalis6@gmail.com',
