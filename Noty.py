a819405485404ca49c07f86309b2cb9bb28729fc
Giorgos Balis <gmpalis6@gmail.com>
2017-06-02 11:08:48 +0200

    Edited Noty.py file.

--- Noty.py
+++ Noty.py
@@ -1,19 +1,19 @@
 def cli():
-	import time
-	from Tkinter import *
+	import time 
+	from Tkinter import * #imports time library and Tkinter library.
 
     
-	
 
-	print("Welcome to Noty.You can now create sticky notes, easily.")
+
+	print("Welcome to Noty.You can now create sticky notes, easily.") #Prints the text.
 	time.sleep(2)
-	x=raw_input("Type your notes here: ")
+	x=raw_input("Type your notes here: ") #Asks for user input.
 	time.sleep(1)
 	root = Tk()
-	root.title("Noty")
-	root.geometry("400x400+50+50")
-	Label = Label(root, text=x)
-	Label.pack()
+	root.title("Noty") #Changes the GUI title to Noty.
+	root.geometry("400x400+50+50") #Changes the width and the height of the GUI.
+	Label = Label(root, text=x) #Prints the previous input(x)
+	Label.pack() #Packs the label
 	root.mainloop()
 
 
