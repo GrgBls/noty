5195bd5fe6233ba42304917a6230b9399cc2916d
Giorgos Balis <grgbls647@gmail.com>
2018-04-24 22:24:04 +0200

    v0.9.2

--- setup.py
+++ setup.py
@@ -3,29 +3,16 @@ from setuptools import setup
 setup(
     name = 'noty',
     packages=["noty"],
-    version = '0.9.0',
+    version = '0.9.2',
     description = 'Creating sticky notes has never been easier',
+    long_desciption = open('README.md').read(),
     author = 'GrgBls',
     author_email = 'grgbls647@gmail.com',
-    url = 'https://github.com/GrgBls/noty', 
+    url = 'https://github.com/GrgBls/noty',
     py_modules=['noty'],
-    install_requires=[
-        
-    ],
+    install_requires=[],
     entry_points='''
         [console_scripts]
         noty=noty.noty:cli
     ''',
 )
-
-
-
-
-
-
-
-
-
-
-
-
