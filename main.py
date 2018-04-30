b88876502ca5a9c9ef3786ee254512627498105b
GrgBls <grgbls647@gmail.com>
2018-04-30 16:35:35 +0200

    Edited code

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from tkinter import *
+import tkinter
 
 def cli():
 	import time
 	current_time = time.strftime("%H:%M:%S")
-	#gets the current time
+	#gets the current time.
 	print("Welcome to Noty.You can now create sticky notes, easily.")
 	time.sleep(1)
-	x = input("Type your notes here: ")
+	note = input("Type your notes here: ")
 	time.sleep(1)
 	#time.sleep prevents GUI from popping up before it receives input.
 	root = Tk()
@@ -15,6 +15,7 @@ def cli():
 	#changes the width and height of the GUI.
 	Label(root, text=current_time).pack()
 	#prints the current time.
-	Label(root, text=x).pack()
+	Label(root, text=note).pack()
 	#prints the input.
 	root.mainloop()
+	#keeps showing the note, until the user closes it.
